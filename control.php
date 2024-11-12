78a87fb85328c92631ecad42fb6e77870c54a59e
Elvan 3312311004 <elvanlee2004@gmail.com>
2024-11-12 22:01:08 +0700

    Navbar & Assets

---
+++ control.php
@@ -0,0 +1,30 @@
+<!-- Nama File: control.php -->
+<!-- Deskripsi: File ini digunakan untuk menyala-matikan lampu -->
+<!-- Dibuat oleh: Elvan - NIM: 3312311004 -->
+<!-- Tanggal: 12 November 2024 -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+    <title>Control</title>
+</head>
+<body>
+    <nav>
+        <div class="logo">
+            <img src="image/logo.png" alt="Logo" />
+            <p>POWER MONITORING</p>
+        </div>
+        <ul>
+            <li><a href="dashboard.php">Dashboard</a></li>
+            <li><a class="active" href="control.php">Control</a></li>
+            <li><a href="history.php">History</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
---
+++ history.php
@@ -0,0 +1,30 @@
+<!-- Nama File: history.php -->
+<!-- Deskripsi: File ini menampilkan riwayat pemakaian daya secara rinci -->
+<!-- Dibuat oleh: Elvan - NIM: 3312311004 -->
+<!-- Tanggal: 12 November 2024 -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+    <title>History</title>
+</head>
+<body>
+    <nav>
+        <div class="logo">
+            <img src="image/logo.png" alt="Logo" />
+            <p>POWER MONITORING</p>
+        </div>
+        <ul>
+            <li><a href="dashboard.php">Dashboard</a></li>
+            <li><a href="control.php">Control</a></li>
+            <li><a class="active" href="history.php">History</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
